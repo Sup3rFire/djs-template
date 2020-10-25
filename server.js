d4f244d13267cbe942e386a1b5154c27a7a61158
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2020-10-25 10:56:47 +0700

    Version 1
    The first version of this discord.js template

---
+++ server.js
@@ -0,0 +1,44 @@
+// Setup
+
+const Discord = require('discord.js'),
+    client = new Discord.Client(),
+    fs = require("fs"),
+    dir = './commands/'
+    Keyv = require('keyv');
+
+require('dotenv').config();
+
+client.blacklist = new Keyv(`sqlite://db/blacklist.sqlite`);
+client.blacklist.on('error', err => console.error('Blacklist Keyv connection error:', err));
+
+client.color = "#FF6464";
+client.prefix = ['.'];
+
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        
+        if (!file.endsWith(".js")) return;
+
+        const event = require(`./events/${file}`);
+
+        let eventName = file.split(".")[0];
+
+        client.on(eventName, event.bind(null, client, Discord));
+        console.log(`Loaded Event: ${eventName}`);
+    });
+});
+
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
+fs.readdirSync(dir).forEach(dirs => {
+    const commands = fs.readdirSync(`${dir}${dirs}/`).filter(files => files.endsWith('.js'));
+    for (const file of commands) {
+        const command = require(`${dir}${dirs}/${file}`);
+        client.commands.set(command.info.name, command);
+        console.log(`Loaded Command: ${command.info.name} (${dirs})`);
+    }
+});
+
+client.login(process.env.TOKEN);
